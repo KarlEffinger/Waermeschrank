fae0ba9d47ce730d3d4b275dd6a9284501dbed61
KarlEffinger <kaeff@gmx.de>
2025-12-03 18:44:38 +0100

    Update lv_conf.h

--- lv_conf.h
+++ lv_conf.h
@@ -3,21 +3,44 @@
 
 #define LV_COLOR_DEPTH 16
 #define LV_USE_LOG 0
+
+// Widgets
 #define LV_USE_LABEL 1
-#define LV_USE_BTN 1
-#define LV_USE_SLIDER 1
-#define LV_USE_ARC 1
-#define LV_USE_SWITCH 1
-#define LV_USE_BAR 1
-#define LV_USE_TEXTAREA 1
-#define LV_USE_KEYBOARD 1
-#define LV_USE_LIST 1
-#define LV_USE_DROPDOWN 1
-#define LV_USE_CANVAS 1
-#define LV_USE_IMG 1
-#define LV_USE_LINE 1
-#define LV_USE_OBJ_REALIGN 1
-#define LV_USE_ANIMATION 1
+#define LV_USE_BTN 0
+#define LV_USE_SLIDER 0
+#define LV_USE_ARC 0
+#define LV_USE_SWITCH 0
+#define LV_USE_BAR 0
+#define LV_USE_TEXTAREA 0
+#define LV_USE_KEYBOARD 0
+#define LV_USE_LIST 0
+#define LV_USE_DROPDOWN 0
+#define LV_USE_CANVAS 0
+#define LV_USE_IMG 0
+#define LV_USE_LINE 0
+
+// EXTRA WIDGETS
+#define LV_USE_ANIMIMG 0               
+#define LV_USE_CALENDAR 0              
+#define LV_USE_CHART 0                 
+#define LV_USE_COLORWHEEL 0             
+#define LV_USE_IMGBTN 0                
+#define LV_USE_KEYBOARD 0               
+#define LV_USE_LED 0                   
+#define LV_USE_LIST 0                   
+#define LV_USE_MENU 0                  
+#define LV_USE_METER 0                  
+#define LV_USE_MSGBOX 0                 
+#define LV_USE_SPAN 0                   
+#define LV_USE_SPINBOX 0                
+#define LV_USE_SPINNER 0                
+#define LV_USE_TABVIEW 0                
+#define LV_USE_TILEVIEW 0               
+#define LV_USE_WIN 0                    
+
+// Features
+#define LV_USE_OBJ_REALIGN 0
+#define LV_USE_ANIMATION 0
 #define LV_USE_THEME_DEFAULT 1
 
 #define LV_FONT_MONTSERRAT_16 1
